6ba3ae3b7ae5f25caff51b149e2e80e2d535398c
jatulya <atulyajayakrishnan@gmail.com>
2025-06-15 18:17:57 +0530

    Button variants added

--- src/pages/BookCatalog.tsx
+++ src/pages/BookCatalog.tsx
@@ -1,5 +1,10 @@
+import Button from "../components/button/Button";
+
 export default function BookCatalog (){
     return (
     <>
+        <Button type="button" variant ={{ color : "primary", size : 'lg'}}>
+            Back to Home
+        </Button>
     </>)
 }
