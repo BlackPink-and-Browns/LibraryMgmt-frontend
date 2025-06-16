bdc553656607fb2a710995a69f0e7b2635dd23ef
Adithya-Sailesh <mailatadithya@gmail.com>
2025-06-16 12:36:18 +0530

    minorchange

--- src/components/adminsidebar/AdminSideBar.tsx
+++ src/components/adminsidebar/AdminSideBar.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./AdminSideBar.css"
 import Button from '../Button'
 import SidebarNavButton from '../SidebarNavButton'
-import { Book, Home, ShieldOff, Warehouse } from 'lucide-react'
+import { Book, Home, ShieldOff, User, Warehouse } from 'lucide-react'
 const AdminSideBar = () => {
   return (
     <div className=' h-full bg-white/80 backdrop-blur-sm shadow pl-5 py-6 text-gray-600'>
@@ -11,7 +11,7 @@ const AdminSideBar = () => {
             <SidebarNavButton label="Dashboard" to="/admin" Icon={Home} />
             <SidebarNavButton label="Books" to="/admin/books" Icon={Book} />
             <SidebarNavButton label="Shelf" to="/admin/shelf" Icon={Warehouse} />
-            <SidebarNavButton label="Books" to="/admin/books" Icon={Book} />
+            <SidebarNavButton label="Users" to="/admin/books" Icon={User} />
         </div>
     </div>
    
