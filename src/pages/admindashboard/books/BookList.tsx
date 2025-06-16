b9aaf7f22904e1c1ba8da31de745e92de57481b1
Adithya-Sailesh <mailatadithya@gmail.com>
2025-06-17 02:11:30 +0530

    minorchange

--- src/pages/admindashboard/books/BookList.tsx
+++ src/pages/admindashboard/books/BookList.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import AdminBookTile from "../../../components/AdminItemTile";
+
 import {bookDb} from "../../../data";
 import AdminItemTile from "../../../components/AdminItemTile";
 
@@ -11,7 +11,6 @@ const BookList = () => {
   return (
     <div className="max-w-4xl mx-auto mt-10 space-y-4 bg-white p-5 shadow-md rounded-xl">
       {books.map((book) => (
-        // <AdminBookTile key={index} book={book} onClick={() => navigate(`/admin/books/book-list/${book.isbn}`)}  />
         <AdminItemTile item={book} type="book" onClick={() => navigate(`/admin/books/book-list/${book.isbn}`)} />
 
       ))}
