cd97a311923af828862a646066be74e300ec9dab
richa_teresa <gdscrichateresamohan@gmail.com>
2025-06-18 12:39:53 +0530

    feat:logout button

--- src/components/DashboardNavButtons.tsx
+++ src/components/DashboardNavButtons.tsx
@@ -1,24 +1,28 @@
 import React from "react";
 import { useNavigate, useLocation, useParams } from "react-router-dom";
 import Button from "./Button";
-import { LayoutDashboard, Search, User } from "lucide-react";
+import { LogOut, Search, User } from "lucide-react";
 
 export default function DashboardNavButtons() {
   const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
-
+  const onLogout=()=>{
+        localStorage.setItem("token","")
+        localStorage.setItem("isLoggedIn","false")
+        navigate("/",{replace:true})
+    }
 
 
   return (
     <div className="flex gap-4 items-center">
       <Button
         variant={{ color:"primary", size: "medium" }}
-        type="button"
-        onClick={() => navigate("")}
+        type="submit"
+        onClick={onLogout}
       >
         <div className="flex flex-row items-center justify-center text-white-500">
-          <LayoutDashboard/>
-        <p >DashBoard</p>
+          <LogOut/>
+        <p >Logout</p>
         </div>
       </Button>
 
--- src/pages/employeedashboard/EmployeeDashboard.tsx
+++ src/pages/employeedashboard/EmployeeDashboard.tsx
@@ -72,7 +72,7 @@ const stats: StatCardProps[] = [
       >
         <DashboardNavButtons />
       </Header>
-      <main className="px-40 py-10 min-h-screen bg-[#f3f4f6] ">
+      <main className="px-40 py-10 min-h-screen  ">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
           {stats.map((stat, index) => (
             <StatCard
