f9c47fa6648a18bcc3a340371c52bc487f1ae578
J-Atulya <atulyajayakrishnan@gmail.com>
2025-06-19 08:03:28 +0530

    Added: Shelves in Borrow Button

---
+++ src/api-service/book/request.api.ts
@@ -0,0 +1,19 @@
+import baseApi from "../api";
+
+const requestApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        getRequests: builder.query({
+            query: () => '/requests/books',
+            providesTags: ['REQUEST'],
+        }),
+        
+        removeRequest: builder.mutation({
+            query: (waitlistIds: number[] | null) => ({
+                url: '/requests/books',
+                method: 'PATCH',
+                body: waitlistIds ? { waitlistIds } : null,
+            }),
+            invalidatesTags: ['REQUEST'],
+        }),
+    }),
+})
